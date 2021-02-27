4b473fa5354ab97dc0bad73f1d4b785ba9aa4d0b
burakhalefoglu <42536490+burakhalefoglu@users.noreply.github.com>
2021-02-27 22:16:19 +0300

    Initial commit

---
+++ CustomerManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    public class CustomerManager
+    {
+        List<Customer> customers = new List<Customer>();
+        public void Add(Customer customer)
+        {
+            customers.Add(customer);
+            Console.WriteLine(customer.Name + "Müşterisi eklendi");
+        }
+
+        public void Delete(Customer customer)
+        {
+            var itemToRemove = customers.Single(r => r.Name == customer.Name);
+            customers.Remove(itemToRemove);
+            Console.WriteLine(customer.Name + "Müşterisi silindi");
+        }
+
+        public void List()
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Müşterimizin adı: " + customer.Name);
+
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new Customer
+            {
+                id=1,
+                Name="burak"
+            });
+
+            customerManager.Add(new Customer
+            {
+                id = 2,
+                Name = "ibrahim"
+            });
+            customerManager.List();
+            customerManager.Delete(new Customer
+            {
+                id = 2,
+                Name = "ibrahim"
+            });
+            customerManager.List();
+            Console.ReadLine();
+        }
+    }
+}
